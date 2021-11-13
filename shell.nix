eb3bab6729f20c8580f0595129ef6b06e231ad9c
Aumy <aumy@outlook.jp>
2021-11-13 22:54:22 +0900

    init

---
+++ shell.nix
@@ -0,0 +1,11 @@
+{ pkgs ? import <nixpkgs> { } }: pkgs.mkShell {
+  buildInputs = [
+    pkgs.ocaml
+    pkgs.ocamlPackages.findlib
+    pkgs.dune_2
+    pkgs.ocamlPackages.ocaml-lsp
+    pkgs.ocamlPackages.utop
+    pkgs.ocamlformat
+    pkgs.ocamlPackages.ocamlformat-rpc-lib
+  ];
+}
