dde3015887230703212c9c08d771d6f9c421df16
Aumy <aumy@outlook.jp>
2022-04-07 05:33:42 +0900

    flake.nixを整頓

--- flake.nix
+++ flake.nix
@@ -1,22 +1,18 @@
 {
-  description = "A very basic flake";
+  description = "An OCaml-like language implementation";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.opal = {
-    flake = false;
-    url = "github:pyrocat101/opal/v0.1.1";
-  };
 
-  outputs = { self, nixpkgs, flake-utils, opal }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system}; in
       rec {
         packages = flake-utils.lib.flattenTree
           {
-            mini-ocaml = pkgs.callPackage ./default.nix { };
+            mini-camlish = pkgs.callPackage ./default.nix { };
           };
-        defaultPackage = packages.mini-ocaml;
+        defaultPackage = packages.mini-camlish;
 
         checks = {
           test = pkgs.runCommand "dune-test"
@@ -40,8 +36,8 @@
           '';
         };
 
-        apps.mini-ocaml = flake-utils.lib.mkApp { drv = packages.mini-ocaml; };
-        defaultApp = apps.mini-ocaml;
+        apps.mini-camlish = flake-utils.lib.mkApp { drv = packages.mini-camlish; };
+        defaultApp = apps.mini-camlish;
         devShell = pkgs.callPackage ./shell.nix { };
       }
     );
